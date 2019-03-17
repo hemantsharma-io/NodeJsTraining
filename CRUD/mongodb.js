12ceba773a731fec05e096bdd68da63f553d20ca
Hemant win <hemantsharma.pune@gmail.com>
2019-03-17 22:47:25 +0530

    CRUD operations

---
+++ CRUD/mongodb.js
@@ -0,0 +1,16 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const database = 'task-manager';
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=> {
+    if(error){
+        return console.log('problem connection mongodb'+ error);
+    }
+    const db = client.db(database);
+    db.collection('users').insertOne({
+        'name':'Hemant Sharma',
+        'role':'Architect'
+    });
+});
